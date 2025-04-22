9f8beab1f7998f727883110fb1383c30f3e19f4f
Warrior of Zarona <andrewbautista85@gmail.com>
2025-04-22 11:10:27 -0400

    adds new favicon and updates title, adds logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,10 @@ export default function Navbar() {
       <div className='bg-black/70 backdrop-blur-sm'>
         <div className='container mx-auto px-6 py-4'>
           <div className='flex justify-between items-center'>
-            <div className='text-2xl font-bold text-white'>Andrew B.</div>
-
+            <div className='flex items-end'>
+              <img src='/assets/images/logo.png' className='h-10 w-10' />
+              <div className='text-2xl font-bold text-white'>AMB Webdev</div>
+            </div>
             {/* Mobile menu button */}
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
